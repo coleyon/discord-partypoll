ff35dec70c4eda33bac61a2c2a0edb9ef0c5582a
coleyon <coleyon784@gmail.com>
2020-09-13 23:20:01 +0900

    implement limit

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ import string
 bot = commands.Bot(command_prefix="/")
 
 RE_LIMIT = r"^\[(\d+)\].+$"
-RE_CURRENT_LIMIT = r"^.+\((\d+)/(\d+)\).+$"
-RE_EMOJI = r"^(:.+:)"
+# RE_CURRENT_LIMIT = r"^.+\((\d+)/(\d+)\).+$"
+# RE_EMOJI = r"^(:.+:)"
 # RE_EMOJI_UNICODE = re.compile("[\U00010000-\U0010ffff]", flags=re.UNICODE)
 ORG_EMOJIS = ["1⃣", "2⃣", "3⃣", "4⃣", "5⃣", "6⃣", "7⃣", "8⃣", "9⃣", "🔟"]
 EMOJIS = {k: v for k, v in zip(range(0, len(ORG_EMOJIS)), ORG_EMOJIS)}
 RE_EMBED_LINE = r"(^.+\()(\d+)(/)([\d|-]+)(\).+$)"
+COLOR = discord.Colour.magenta()
 
 
 @bot.event
@@ -33,9 +34,7 @@ async def echo(ctx):
     await ctx.channel.send(ctx.message.content)
 
 
-@bot.event
-async def on_reaction_add(reaction, user):
-    # author = reaction.message.author
+async def _renew_reaction(reaction, user, is_remove=False):
     if user.id == bot.user.id:
         return
     old_embed = reaction.message.embeds[0]
@@ -44,44 +43,37 @@ async def on_reaction_add(reaction, user):
 
     # update current member count
     tmp = list(re.match(RE_EMBED_LINE, desc[key]).groups())
+    limit = int(tmp[3]) if tmp[3].isnumeric() else -1
+    if not is_remove and limit >= 0 and reaction.count - 1 > limit:
+        # over the limit when reaction added
+        await reaction.message.remove_reaction(reaction.emoji, user)
+        await reaction.message.channel.send(
+            "{mention} さんへ\n{e} はもう満員だったので、今のリアクションを取り消しました。ごめんなさい:sob:\n{poll_url}".format(
+                mention=user.mention,
+                e=reaction.emoji,
+                poll_url=reaction.message.jump_url,
+            )
+        )
+        return
+
     tmp[1] = str(reaction.count - 1)
     new_line = "".join(tmp)
     desc[key] = new_line
     # update current
-    new_embed = Embed(title=old_embed.title, description="\n".join(desc.values()))
+    new_embed = Embed(
+        title=old_embed.title, description="\n".join(desc.values()), color=COLOR
+    )
     await reaction.message.edit(embed=new_embed)
+
+
+@bot.event
+async def on_reaction_add(reaction, user):
+    await _renew_reaction(reaction, user)
 
 
 @bot.event
 async def on_reaction_remove(reaction, user):
-    # if user.id == bot.user.id:
-    #     return
-    # message = reaction.message
-    # channel = reaction.message.channel
-    # target_count = [r for r in message.reactions if r.emoji == reaction.emoji][
-    #     0
-    # ].count - 1
-    # await channel.send(
-    #     "{u}さんがmessageid={id}からリアクション{r}をはずし、リアクション者数は{c}になりました。".format(
-    #         u=user.display_name, id=message.id, r=reaction.emoji, c=target_count
-    #     )
-    # )
-    # # TODO message.edit(embed=message.embeds[0].description + "内容に変更を加えるテスト")
-    # await message.edit(content="メッセージの内容を書き替えるテスト")
-    if user.id == bot.user.id:
-        return
-    old_embed = reaction.message.embeds[0]
-    desc = {n: s for n, s in enumerate(old_embed.description.split("\n"))}
-    key = [s[0] for s in desc.items() if reaction.emoji in s[1]][0]
-
-    # update current member count
-    tmp = list(re.match(RE_EMBED_LINE, desc[key]).groups())
-    tmp[1] = str(reaction.count - 1)
-    new_line = "".join(tmp)
-    desc[key] = new_line
-    # update current
-    new_embed = Embed(title=old_embed.title, description="\n".join(desc.values()))
-    await reaction.message.edit(embed=new_embed)
+    await _renew_reaction(reaction, user, is_remove=True)
 
 
 def _get_limit(msg):
@@ -103,9 +95,7 @@ async def make_poll(ctx, title, *args):
         for num, msg in enumerate(args)
     }
     embed = discord.Embed(
-        title=title,
-        description="\n".join(contents.values()),
-        color=discord.Colour.magenta(),
+        title=title, description="\n".join(contents.values()), color=COLOR
     )
     message = await ctx.channel.send("", embed=embed)
     for num, _ in enumerate(args):
